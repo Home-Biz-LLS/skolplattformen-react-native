3f28210e0acde03b58dd8ef25ecca94daa146172
Lee <leesheppard2404@gmail.com>
2023-09-28 14:39:41 +0200

    feat(installed webview to load notification external links)
    Co-authored-by: Sebastian Palmqvist <PalmN72@users.noreply.github.com>
    Co-authored-by: Lvan Ni <lvan-ni@users.noreply.github.com>

--- app/components/notification.component.tsx
+++ app/components/notification.component.tsx
@@ -1,34 +1,33 @@
 import {Notification as NotificationType} from '../libs/api/lib';
-// import {StyleService, Text, useStyleSheet} from '@ui-kitten/components';
-// import moment from 'moment';
-// import React from 'react';
-import {TouchableOpacity, View, Text} from 'react-native';
-// import {Layout, Sizing, Typography} from '../styles';
-// import {ModalWebView} from './modalWebView.component';
+import {StyleService, Text, useStyleSheet} from '@ui-kitten/components';
+import moment from 'moment';
+import React from 'react';
+import {TouchableOpacity, View} from 'react-native';
+import {Layout, Sizing, Typography} from '../styles';
+import {ModalWebView} from './modalWebView.component';
 
 interface NotificationProps {
   item: NotificationType;
 }
 
 export const Notification = ({item}: NotificationProps) => {
-  // const styles = useStyleSheet(themedStyles);
-  // const [isOpen, setIsOpen] = React.useState(false);
-  // const open = () => setIsOpen(true);
-  // const close = () => setIsOpen(false);
+  const styles = useStyleSheet(themedStyles);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
 
-  // const date = item.dateModified || item.dateCreated;
-  // const displayDate = date ? moment(date).fromNow() : null;
+  const date = item.dateModified || item.dateCreated;
+  const displayDate = date ? moment(date).fromNow() : null;
 
-  // const sharedCookiesEnabled = Boolean(
-  //   item.url &&
-  //     (item.url.startsWith('https://start.unikum.net/') ||
-  //       item.url.startsWith('https://hjarntorget.goteborg.se')),
-  // );
+  const sharedCookiesEnabled = Boolean(
+    item.url &&
+      (item.url.startsWith('https://start.unikum.net/') ||
+        item.url.startsWith('https://hjarntorget.goteborg.se')),
+  );
 
   return (
     <>
-      <Text>notification</Text>
-      {/* <TouchableOpacity onPress={open}>
+      <TouchableOpacity onPress={open}>
         <View style={styles.card}>
           <View>
             <Text style={styles.title}>{item.sender}</Text>
@@ -47,27 +46,27 @@ export const Notification = ({item}: NotificationProps) => {
           onClose={close}
           sharedCookiesEnabled={sharedCookiesEnabled}
         />
-      )} */}
+      )}
     </>
   );
 };
 
-// const themedStyles = StyleService.create({
-//   card: {
-//     ...Layout.flex.full,
-//     borderRadius: 15,
-//     paddingVertical: Sizing.t4,
-//     paddingHorizontal: Sizing.t4,
-//     marginBottom: Sizing.t3,
-//     backgroundColor: 'background-basic-color-1',
-//   },
-//   title: {
-//     ...Typography.header,
-//     marginBottom: Sizing.t1,
-//   },
-//   subtitle: {
-//     ...Typography.fontSize.xs,
-//     color: 'text-hint-color',
-//     marginBottom: Sizing.t2,
-//   },
-// });
+const themedStyles = StyleService.create({
+  card: {
+    ...Layout.flex.full,
+    borderRadius: 15,
+    paddingVertical: Sizing.t4,
+    paddingHorizontal: Sizing.t4,
+    marginBottom: Sizing.t3,
+    backgroundColor: 'background-basic-color-1',
+  },
+  title: {
+    ...Typography.header,
+    marginBottom: Sizing.t1,
+  },
+  subtitle: {
+    ...Typography.fontSize.xs,
+    color: 'text-hint-color',
+    marginBottom: Sizing.t2,
+  },
+});
